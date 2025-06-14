7e28389a9c8e3ff76c012a8ecf375cec4e5cfc3e
Fahim Montasir <montasirtuhin@gmail.com>
2025-06-14 10:21:10 +0600

    minor changes

--- src/Database.java
+++ src/Database.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3307/birthday";
     private static final String USER = "root";
-    private static final String PASS = "Fahim.1128@";
+    private static final String PASS = "";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
